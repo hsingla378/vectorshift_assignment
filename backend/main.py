98e3e2b5279d634e628238f44de6fd27f39ee1b1
Himanshu Singla <himanshusingla@Himanshus-MacBook-Pro.local>
2024-09-18 00:42:17 +0530

    allow all origins

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Add the CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://vectorshift-assignment-delta.vercel.app"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
