38131ed863b6119f6d67f7656117cb59088ee2e4
Himanshu Singla <himanshusingla@Himanshus-MacBook-Pro.local>
2024-09-18 00:02:06 +0530

    resolve cors error

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Add the CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["http://localhost:3000", "https://vectorshift-assignment-delta.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
